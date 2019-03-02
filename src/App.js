224ad489c18ea638502a928a9fcb3dbdd8e62db7
Grace Hartzell <gracehartzell@utexas.edu>
2019-03-02 13:10:19 -0600

    Router set up, rendering route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">File System Viewer</header>
+        <Router>
+          <Route
+            path="/"
+            render={() => <h1 style={{ color: "red" }}>Has rendered!</h1>}
+          />
+        </Router>
       </div>
     );
   }
