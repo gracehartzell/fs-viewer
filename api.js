718767ecaaac1051c8d080050a1309d7a54f2f02
Grace Hartzell <gracehartzell@utexas.edu>
2019-03-06 15:00:59 -0600

    Remove console.log

--- api.js
+++ api.js
@@ -1,6 +1,4 @@
 const { Folder } = require("./models");
-const util = require("util");
-const chalk = require("chalk");
 
 module.exports = {
   folderApi: (req, res) => {
@@ -8,10 +6,6 @@ module.exports = {
     const pathArray = path.split("/").splice(1);
 
     const folderContent = Folder.getFolderContent(pathArray);
-    console.log(
-      "Check folder content: ",
-      chalk.bold.blue(util.inspect(folderContent, { deep: 1 })),
-    );
     return res.status(200).json(folderContent);
   },
 };
